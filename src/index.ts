071b90f85dc253b1431340896d8c58f8ec8525fb
Artem Igonin <officialigonin@mail.ru>
2025-04-23 19:20:41 +0400

    edit games, add new game, add commands

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,30 @@
 import {Markup, Telegraf} from "telegraf";
 import {message} from "telegraf/filters";
+import {
+  ChatMemberAdministrator,
+  ChatMemberOwner,
+} from "telegraf/typings/core/types/typegram";
 
 const tkn = process.env.USER_FATHER_TKN || "";
-const helpDescription = process.env.BOT_HELP || "test";
+const helpDescription = process.env.USER_BOT_HELP || "...";
 
 const luckyBot = new Telegraf(tkn);
+let currentRandomNumber: number | null = null;
+let isRockPaperGameActive = false;
 
-// discountBot.start(ctx => ctx.reply("Very welcome"));
-// discountBot.on(message("animation"), ctx => ctx.reply("👍"));
-// discountBot.on(message("sticker"), ctx => ctx.reply("👍"));
-// discountBot.on(message("contact"), ctx => ctx.reply("👍"));
-// discountBot.on(message("photo"), ctx => ctx.reply("I can't read it"));
-// discountBot.on(message("voice"), ctx => ctx.reply("I can't hear it"));
+const Emojies = {
+  "🤚": 1,
+  "🖐": 1,
+  "✌️": 2,
+  "✊": 3,
+};
 
-// //добавить библу на матные слова
-// discountBot.hears(["hi", "ho", "hahaha", "go"], ctx => ctx.reply("Hey there"));
+let rockPaperPlayersAndResults: {
+  player: string;
+  emoji: string;
+  value: number;
+}[] = [];
 
-//обработка вводимого продукта
-// type TShops = "x5Res" | "magnitRes" | "guliverRes";
-// interface ISearchDiscountResult {}
-// discountBot.on(message("text"), async ctx => {
-//   console.log(ctx, " context current");
-//   await ctx.telegram.sendMessage(ctx.message.chat.id, `Ща поищем...`);
-//   //   try {
-//   //     const [x5Res, magnitRes, guliverRes] = await Promise.allSettled([
-//   //       apiRequest<any>(x5Api,{params:{
-//   //         mode:'delivery',
-//   //         limit:100,
-//   //         q:
-//   //       }}),
-//   //       apiRequest<any>("/api/v1/calculations_lvl2"),
-//   //       apiRequest<any>("/api/v1/calculations_lvl3"),
-//   //     ]);
-//   //     console.log(x5Res, "x5Res");
-//   //   } catch (error) {
-//   //     throw new Error(`Error with search discount - ${error}`);
-//   //   }
-// });
-
-// const randonNumScene = new WizardScene(
-//   "randomNum",
-//   ctx => {
-//     let keyboard_buttons = Markup.keyboard(["Я водитель"]).oneTime().resize();
-//     ctx.reply("Кто вы?", keyboard_buttons);
-//     return ctx.wizard.next();
-//   },
-//   ctx => {
-//     ctx.reply(`Я грут`);
-//     return ctx.wizard.next();
-//   },
-// );
-// const stages= new Stage([randonNumScene])
 const gamesKeyboard = [
   [
     {
@@ -59,6 +33,7 @@ const gamesKeyboard = [
     },
     {text: "Dice", callback_data: "diceGame"},
   ],
+  [{text: "Rock, Paper, Scissors", callback_data: "rockPaperGame"}],
   [
     {
       text: "Close",
@@ -66,13 +41,21 @@ const gamesKeyboard = [
     },
   ],
 ];
-let currentRandomNumber: number | null = null;
 luckyBot.start(ctx => {
+  if (isRockPaperGameActive || currentRandomNumber) {
+    ctx.reply("Начата другая игра");
+    return;
+  }
+
   ctx.reply("Well, let`s play", Markup.inlineKeyboard(gamesKeyboard));
 });
 luckyBot.help(ctx => ctx.reply(helpDescription));
-
+//commands
 luckyBot.command("games", ctx => {
+  if (isRockPaperGameActive || currentRandomNumber) {
+    ctx.reply("Начата другая игра");
+    return;
+  }
   ctx.reply("Games", Markup.inlineKeyboard(gamesKeyboard));
 });
 luckyBot.command("show_number", ctx => {
@@ -82,7 +65,34 @@ luckyBot.command("show_number", ctx => {
       : "Не покажу",
   );
 });
+luckyBot.command("reset", ctx => {
+  currentRandomNumber = null;
+  isRockPaperGameActive = false;
+  rockPaperPlayersAndResults = [];
+  ctx.reply("Состояние обнулено");
+});
+luckyBot.command("lucker", async ctx => {
+  const membersList = await luckyBot.telegram.getChatAdministrators(
+    ctx.update.message.chat.id,
+  );
 
+  const shortUsersInfo = membersList.map(
+    (item: ChatMemberOwner | ChatMemberAdministrator) => item.user,
+  );
+
+  shortUsersInfo.shift();
+  const randomIndex = Math.floor(Math.random() * shortUsersInfo.length);
+
+  ctx.reply(`Везунчик - @${shortUsersInfo[randomIndex].username}`);
+});
+//actions
+luckyBot.action("rockPaperGame", ctx => {
+  isRockPaperGameActive = true;
+  ctx.reply(
+    'Игра "Камень-Ножницы-Бумага" началась. Принимаются смайлы ✊, ✌️, 🖐, 🤚. Для сохранения интереса требуется оборачивать смайлы в ||. Пример: ||✊||.',
+  );
+  ctx.editMessageReplyMarkup({inline_keyboard: []});
+});
 luckyBot.action("randomNumber", ctx => {
   currentRandomNumber = Math.floor(Math.random() * 10);
   ctx.reply(`Случайно число загадано`);
@@ -98,6 +108,65 @@ luckyBot.action("closeKeyboard", ctx => {
   ctx.editMessageReplyMarkup({inline_keyboard: []});
 });
 
+//functions
+luckyBot.hears(["✊", "✌️", "🖐", "🤚"], async ctx => {
+  const currentPlayerNick = ctx.update.message.from.username;
+  const isAlreadyHas = rockPaperPlayersAndResults.some(
+    el => el.player === currentPlayerNick,
+  );
+  if (isAlreadyHas) {
+    ctx.reply(`@${currentPlayerNick} не жульничай!`);
+    return;
+  }
+
+  rockPaperPlayersAndResults.push({
+    player: currentPlayerNick || "",
+    emoji: ctx.update.message.text,
+    value: Emojies[ctx.update.message.text as keyof typeof Emojies],
+  });
+  if (rockPaperPlayersAndResults.length >= 1) {
+    ctx.reply(
+      "Завершить?",
+      Markup.keyboard(["Завершить камень,ножницы,бумага"]).resize(),
+    );
+  }
+});
+
+luckyBot.hears("Завершить камень,ножницы,бумага", async ctx => {
+  ctx.reply("Подведем итоги");
+  const emojiValues = Array.from(
+    new Set(rockPaperPlayersAndResults.map(item => item.value)),
+  );
+  if (emojiValues.length !== 2) {
+    rockPaperPlayersAndResults = [];
+    setTimeout(() => ctx.reply("Нужно переиграть"), 1000);
+    return;
+  }
+  let winners: {player: string; emoji: string}[] = [];
+  rockPaperPlayersAndResults.forEach(el => {
+    //ножницы
+    if (!emojiValues.includes(3)) {
+      el.value === 2 && winners.push({player: el.player, emoji: el.emoji});
+      return;
+    }
+    //бумага
+    if (!emojiValues.includes(2)) {
+      el.value === 1 && winners.push({player: el.player, emoji: el.emoji});
+      return;
+    }
+    //камень
+    if (!emojiValues.includes(1)) {
+      el.value === 3 && winners.push({player: el.player, emoji: el.emoji});
+      return;
+    }
+  });
+  winners.forEach(winner => {
+    ctx.reply(`Победил(а) @${winner.player} : ${winner.emoji}`);
+  });
+  //end
+  rockPaperPlayersAndResults = [];
+  isRockPaperGameActive = false;
+});
 luckyBot.hears(["Покажи число", "покажи число"], ctx => {
   ctx.reply(
     currentRandomNumber
@@ -108,7 +177,6 @@ luckyBot.hears(["Покажи число", "покажи число"], ctx => {
 
 luckyBot.on(message("text"), ctx => {
   if (ctx.update.message.text == String(currentRandomNumber)) {
-    console.log("test", currentRandomNumber);
     ctx.reply(
       `@${ctx.update.message.from.username} угадал(а). Число ${currentRandomNumber}`,
     );
